25f0720de4963d01848a94a76e2b2c6a1768929f
Ritika Bhatia <139344947+iam-ritikabhatia@users.noreply.github.com>
2024-01-22 20:09:39 +0530

    Add files via upload

---
+++ Day22.js
@@ -0,0 +1,67 @@
+// // Question 1
+
+// {
+//     var a = "Ram"
+//     var b = "Priyanka"
+// }
+// console.log(a, b)
+
+// // Question 2
+//  let a = 123456789
+//  let b = 123456789n
+//  let c = "false"
+//  let d 
+//  let e = "Ritika"
+//  let f = null
+
+//  let g = typeof a
+//  let h = typeof b
+//  let i = typeof c
+//  let j = typeof d
+//  let k = typeof e
+//  let l = typeof f
+
+//  console.table([g, h, i, j, k, l])
+
+// //   Question 3
+
+// typeof null is an object because object bit number is 00000 and also null bit number is 00000.toExponential
+
+// //  question 4
+
+// let a = 18
+// let a = 19
+// console.log(a)
+
+// error : syntax error
+
+// var a = 18
+// var a = 19
+// console.log(a)
+
+// a = 19 (answer)
+
+// const a = 18
+// a = 19
+// console.log(a)
+
+// error assignment to constant error
+
+
+// const a = 18
+// const a = 19
+// console.log(a)
+
+// there is an error generator
+
+// question 5
+
+// function anyname(){
+//     function hichai(){
+//          a = "chai is my life"
+        
+//     }
+//     hichai ()   
+// }
+// anyname()
+// console.log(a)
